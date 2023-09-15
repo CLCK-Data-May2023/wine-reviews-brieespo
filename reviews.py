c72f4f683d80c58125c3ec0a6c8a0f621da3c2e2
briesposito <elliebe3@gmail.com>
2023-09-14 20:03:50 -0400

    reuploading repo to include csv file

--- reviews.py
+++ reviews.py
@@ -1,2 +1,15 @@
 # add your code here
 
+import pandas as pd
+
+reviews = pd.read_csv("data/winemag-data-130k-v2.csv.zip")
+
+
+df = reviews.groupby('country').agg({'country':'count', 'points':'mean'}).round(1)
+
+df_summary = df.rename(columns = {'country': 'count'})
+
+df_summary = df_summary.sort_values('count', ascending=False) 
+
+
+df_summary.to_csv('data/reviews-per-country.csv')
